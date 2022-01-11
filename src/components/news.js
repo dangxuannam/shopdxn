d56151540af98ecaf566f31c47fa4d3669db31a2
dangxuannam210 <dangxuannam210@gmail.com>
2022-01-11 22:27:24 +0700

    fix: lỗi logo

--- src/components/news.js
+++ src/components/news.js
@@ -9,7 +9,7 @@ const newss = {
          
           ${news.map((post) => `
           <div class="learn border-4 mr-2  ">
-            <img class="m-4" src="${post.img}" alt="" width="340px">
+            <img class="m-4" src="${post.img}" alt="" width="320px" height="320px">
             <h3 class="m-4 text-orange-500 font-bold"><a href="/product/${post.id}" class="font-semibold text-orange-500 text-lg">${post.title}</a></h3>
             <p class="w-80 m-4">${post.dect}</p>
             </div>
--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export default [
     {
-        id: 1, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/300/300", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
+        id: 1, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/320/320", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
 
     },
     {
-        id: 2, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/300/300", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
+        id: 2, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/320/320", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
 
     },
     {
-        id: 2, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/300/300", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
+        id: 2, title: "Vinh danh 295 sinh viên fpt polytechnic học kì summer 2021", img: "https://picsum.photos/320/320", dect: "Tối ngày 20/12/2021 ,Cao đẳng fpt polytechnic đã tổ chức lễ tôn vinh ong vàng tuyên dương 295 sinh viên có kết quả cao nhất.",
 
     },
 
